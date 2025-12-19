6d721dfcd1657ceef843ab7f4df68789695177f0
kratiDatacode <krati.datacode@gmail.com>
2025-12-19 16:54:33 +0530

    Task krati : add admin dashboard

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Client, Project, Contact, Subscriber
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'designation')
+    search_fields = ('name', 'designation')
+    list_filter = ('designation',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'client')
+    search_fields = ('name',)
+    list_filter = ('client',)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'mobile_number', 'city')
+    search_fields = ('full_name', 'email', 'mobile_number', 'city')
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    search_fields = ('email',)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 
 # Create your models here.
-#Projects Table 
-class Projects(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='project_images/')
-
-    def __str__(self):
-        return self.name
 
 #Clients Table
-class Clients(models.Model):
+class Client(models.Model):
     name = models.CharField(max_length=100)
     designation = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='client_images/')
+
+    def __str__(self):
+        return self.name
+        
+#Projects Table 
+class Project(models.Model):
+    client = models.ForeignKey(
+        'Client',
+        on_delete=models.CASCADE,
+        related_name='projects'
+    )
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/')
 
     def __str__(self):
         return self.name
